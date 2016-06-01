4c6543ff5ee76cf7f1bb0fbb4adc62f3043e8820
Darrell Hamilton <darrell.noice@gmail.com>
2016-06-01 10:34:02 -0600

    Yum Plugin

---
+++ README.md
@@ -0,0 +1,12 @@
+## Docker Image Optimizer
+
+A yum plugin to perform the cleanup steps found in [contrib/mkimage-yum.sh](https://github.com/docker/docker/blob/master/contrib/mkimage-yum.sh) every time yum runs.
+
+### Installation
+  - Copy `docker-image-optimizer.py` into the yum plugins directory, `/usr/lib/yum-plugins`
+  - Copy `docker-image-optimizer.conf` into the yum plugins config directory, `/etc/yum/pluginconf.d/`
+  - Ensure plugins are enabled in your yum.conf
+  ```ini
+    [main]
+    plugins=1
+  ```
---
+++ docker-image-optimizer.py
@@ -0,0 +1,42 @@
+import shutil
+import os
+
+from yum.plugins import TYPE_CORE
+
+requires_api_version = '2.3'
+plugin_type = (TYPE_CORE)
+
+delete_dirs = [
+  "/usr/lib/locale",
+  "/usr/share/locale",
+  "/usr/lib/gconv",
+  "/usr/lib64/gconv",
+  "/usr/bin/localedef",
+  "/usr/sbin/build-locale-archive",
+  "/usr/share/man",
+  "/usr/share/doc",
+  "/usr/share/info",
+  "/usr/share/gnome/help",
+  "/usr/share/backgrounds",
+  "/usr/share/wallpapers",
+  "/usr/share/centos-logos",
+  "/usr/share/cracklib",
+  "/var/cache/yum",
+  "/sbin/sln",
+  "/etc/ld.so.cache",
+  "/var/cache/ldconfig",
+]
+
+create_dirs = [
+  "/var/cache/yum",
+  "/var/cache/ldconfig",
+]
+
+def close_hook(conduit):
+  print("Running docker image optimizations...")
+
+  for d in delete_dirs:
+    shutil.rmtree(d, True)
+
+  for d in create_dirs:
+    os.makedirs(d, 0755)
